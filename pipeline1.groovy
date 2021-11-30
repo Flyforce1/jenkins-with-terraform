2a2e21fa0b6370f522b6690297d76253f1a485a1
farrukh90 <farrukhsadykov@gmail.com>
2021-11-30 00:38:07 +0000

    added property

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -1,3 +1,5 @@
+properties([pipelineTriggers([cron('* * * * *')])])
+
 node {
     stage("Clone A Repository") {
         checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class-august.git']]])
