1a2fc3ccceaa4fdccd686e1006eedf31ccada1e7
farrukh90 <farrukhsadykov@gmail.com>
2021-11-30 00:29:30 +0000

    added diff git

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -1,6 +1,6 @@
 node {
     stage("Clone A Repository") {
-        git 'https://github.com/farrukh90/jenkins-class-august.git'
+        checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class-august.git']]])
     }
     stage("Initialize"){
         sh 'terraform init'
